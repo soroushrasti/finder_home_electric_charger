f08697ccaf46c7a168b2e2e70af0bddaa7a0beab
Soroush Rasti <rasti.soroush@gmail.com>
2025-05-24 13:06:18 +0200

    add user APIs

--- src/core/services/internal/db_repository/user.py
+++ src/core/services/internal/db_repository/user.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from src.core.services.models import User
 from sqlalchemy.orm import Session
 
@@ -10,8 +12,8 @@ class UserRepository(UserRepositoryAbstract):
     def __init__(self, db: Session):
         self.db = db
 
-    def get_user_by_id(self, user_id: int) -> User:
-        return self.db.query(User).filter(User.id == user_id).first()
+    def get_user_by_id(self, user_id: int) -> type[User] | None:
+        return self.db.query(User).filter(User.user_id == user_id).first()
 
     def create_user(self, user_data: dict) -> User:
         new_user = User(**user_data)
