76eac80960672091c240c9d8af072573edb5f076
Soroush Rasti <rasti.soroush@gmail.com>
2025-07-29 21:52:48 +0200

    add default value for language

--- src/apis/v1/schemas/user.py
+++ src/apis/v1/schemas/user.py
@@ -25,7 +25,7 @@ class CreateUserRequest(BaseModel):
     postcode_of_home: Optional[str] = None
     user_type: Optional[UserType] = None
     mobile_number: Optional[str]  = None
-    language: Optional[str] = None
+    language: Optional[str] = "English"
 
     @validator('username', pre=True, always=True)
     def set_username_from_email(cls, username, values):
@@ -48,12 +48,12 @@ class ResendVerificationRequest(BaseModel):
     user_id: Optional[int] = None
     email_verification_code: Optional[str] = None
     phone_verification_code: Optional[str] = None
-    language: Optional[str] = None
+    language: Optional[str] = "English"
 
 class ForgotPasswordRequest(BaseModel):
      email: Optional[str] = None
      mobile_number: Optional[str] = None
-     language: Optional[str] = None
+     language: Optional[str] = "English"
 
 class UpdateUserRequest(BaseModel):
     username: Optional[str] = None
