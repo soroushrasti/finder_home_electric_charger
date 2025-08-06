31aaed782d4115a40a220d0f834389790c28a504
Soroush Rasti <rasti.soroush@gmail.com>
2025-08-06 22:52:00 +0200

    fix connection to postgres

--- src/config/database.py
+++ src/config/database.py
@@ -5,9 +5,8 @@ from src.config.base import BaseConfig
 
 # Create SQLite engine - adjust the path as needed
 SQLALCHEMY_DATABASE_URL = BaseConfig().DATABASE_URL
-print(f"{SQLALCHEMY_DATABASE_URL[0:30]}*****")  # Debugging line to check the URL
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
