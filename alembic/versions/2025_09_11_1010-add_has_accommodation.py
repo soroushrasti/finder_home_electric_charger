043259ba01816d59d1f30cd772f21fa7271f88a9
Soroush Rasti <rasti.soroush@gmail.com>
2025-09-11 12:38:17 +0200

    fix delete APIa and add has_accomodation

--- alembic/versions/2025_09_11_1010-add_has_accommodation.py
+++ alembic/versions/2025_09_11_1010-add_has_accommodation.py
@@ -1,6 +1,6 @@
 """add has_accommodation to charging_locations
 
-Revision ID: add_has_accommodation_charging_location
+Revision ID: add_has_accomm
 Revises: add_public_chargers_table
 Create Date: 2025-09-11 10:10:00
 
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = 'add_has_accommodation_charging_location'
+revision = 'add_has_accomm'
 down_revision = 'add_public_chargers_table'
 branch_labels = None
 depends_on = None
@@ -21,4 +21,3 @@ def upgrade() -> None:
 
 def downgrade() -> None:
     op.drop_column('charging_locations', 'has_accommodation')
-
